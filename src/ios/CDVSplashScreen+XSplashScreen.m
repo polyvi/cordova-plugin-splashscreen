889dfb1bcb40094b72e38658e986d1fc451ba575
liumn <liumn@polyvi.com>
2013-12-05 17:53:01 +0800

    [iOS] Added xFace-related implementations to an category.
    1. SplashScreen can be created and initialized many times, however,
    only default app will display engine embedded splash images.
    2. Moved the logic of adding version info from CDVSplashScreen.m to
    the category.

---
+++ src/ios/CDVSplashScreen+XSplashScreen.m
@@ -0,0 +1,75 @@
+/*
+ This file was modified from or inspired by Apache Cordova.
+
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements. See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership. The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License. You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied. See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+
+//
+//  CDVSplashScreen+XSplashScreen.m
+//
+
+#import <xFace/XUtils.h>
+#import <xFace/XVersionLableFactory.h>
+
+#import "CDVSplashScreen.h"
+
+@interface CDVSplashScreen ()
+
+- (void)setVisible:(BOOL)visible;
+
+- (void)updateImage;
+
+@end
+
+@implementation CDVSplashScreen (XSplashScreen)
+
+- (void)pluginInitialize
+{
+    //只有default app才需要显示引擎默认splash
+    if ([XUtils isDefaultAppWebView:self.webView]) {
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:NSSelectorFromString(@"pageDidLoad") name:CDVPageDidLoadNotification object:self.webView];
+        [self setVisible:YES];
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString*)keyPath ofObject:(id)object change:(NSDictionary*)change context:(void*)context
+{
+    [self updateImage];
+    [self addVersionLabel];
+}
+
+- (void)addVersionLabel
+{
+    UILabel *versionLabel = [XVersionLableFactory createWithFrame:_imageView.frame];
+    if(versionLabel)
+    {
+        for (UIView *subview in [_imageView subviews])
+        {
+            if ([subview isKindOfClass:[UILabel class]])
+            {
+                [subview removeFromSuperview];
+                break;
+            }
+        }
+
+        NSAssert(![[_imageView subviews] count], @"Expect no subviews before the version label is added!");
+        [_imageView addSubview:versionLabel];
+    }
+}
+
+@end
